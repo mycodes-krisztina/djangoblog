54a3cfa639fa9e579f63ee0448af99dad7b37d5b
mycodes-krisztina <personalpkhu@gmail.com>
2024-10-23 20:24:48 +0200

    posts app done,more admin options,crud

--- proba/src/posts/admin.py
+++ proba/src/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post  # .models a relatív import
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display=["title","updated","timestamp"]
+    list_display_links=["updated"]
+    list_filter =["updated","timestamp"]
+    search_fields=["title"] #content -> a tartalomban is keres pl.
+    list_editable=["title"] #items you can edit
+    class Meta:
+        model=Post
+
+admin.site.register(Post, PostModelAdmin) #admin függvény,ami regisztrálja a Post modult az admin oldalra
+#így kötődik össze a Post model és a PostModelAdmin
---
+++ proba/src/posts/crud.md
@@ -0,0 +1,12 @@
+Operations on a database:
+
+CREATE - make new; here: add new post
+READ - read data; list, search; here: click on a post, search
+UPDATE - update data;here: edit post
+DELETE - delete data; here: delete post
+
+HTTP methods use these too:
+CREATE == POST
+READ == GET
+UPDATE == PUT/PATCH
+DELETE == DELETE
